91040bb24d4d11e35586cd73a8be2186cb2cba4a
Manoj <123manojverma2005@gmail.com>
2025-10-19 19:39:08 +0530

    sidebar-completed

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,11 @@
+import { Toggle } from "./toggle"
+import { Wrapper } from "./wrapper"
+
+export const Sidebar=()=>{
+    // fetch allowed
+    return (
+        <Wrapper>
+            <Toggle />
+        </Wrapper>
+    )
+}
