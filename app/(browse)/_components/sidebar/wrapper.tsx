b4b24e08b303a863a1268df37a53402046888309
Manoj <123manojverma2005@gmail.com>
2025-10-23 19:26:05 +0530

    Hydration-error solved

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,6 +1,10 @@
 "use client"
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
+import { useIsClient } from "usehooks-ts";
 
 interface WrapperProps{
     children: React.ReactNode;
@@ -9,7 +13,22 @@ interface WrapperProps{
 export const Wrapper=({
     children,
 }:WrapperProps)=>{
+    // const [isClient,setisClient]=useState(false);
+    // either use usestate and useeffect hooks to solve the isclient error or use useIsClient hook from "usehooks-ts" both will work same 
+    const isClient=useIsClient();
+    
     const {collapsed} = useSidebar((state)=>state)
+
+    // useEffect(()=>{
+    //     setisClient(true)
+    // },[])
+    if(!isClient)return (
+        <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+            <ToggleSkeleton />
+            <RecommendedSkeleton />
+        </aside>
+    )
+
     return (
         <aside className={cn(
             "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
