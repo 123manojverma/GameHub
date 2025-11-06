1b497ff73960d19ad1ebe30ef9f6efaa6bd63688
Manoj <123manojverma2005@gmail.com>
2025-11-06 10:14:06 +0530

    User-page done

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -1,15 +1,30 @@
 import { db } from "./db"
 
-export const getUserByUsername=async(username:string)=>{
-    const user=await db.user.findUnique({
-        where:{
+export const getUserByUsername = async (username: string) => {
+    const user = await db.user.findUnique({
+        where: {
             username
         },
-        include:{
-            stream:true,
-            _count:{
-                select:{
-                    followedBy:true
+        select: {
+            id: true,
+            externalUserId: true,
+            username: true,
+            bio: true,
+            imageUrl: true,
+            stream: {
+                select: {
+                    id: true,
+                    isLive: true,
+                    isChatDelayed: true,
+                    isChatEnabled: true,
+                    isChatFollowersOnly: true,
+                    thumbnailUrl: true,
+                    name: true
+                }
+            },
+            _count: {
+                select: {
+                    followedBy: true
                 }
             }
         }
@@ -17,11 +32,11 @@ export const getUserByUsername=async(username:string)=>{
     return user;
 }
 
-export const getUserById=async(id:string)=>{
-    const user=await db.user.findUnique({
-        where:{id},
-        include:{
-            stream:true
+export const getUserById = async (id: string) => {
+    const user = await db.user.findUnique({
+        where: { id },
+        include: {
+            stream: true
         }
     })
     return user;
